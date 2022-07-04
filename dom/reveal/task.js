3b1f68346cfef1052d5960adfad29a918a4185fa
Vladislav Fateev <Fateev_wbd@mail.ru>
2022-07-04 18:22:25 +0500

    123

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,13 +1,13 @@
-const reveal = Array.from(document.getElementsByClassName('reveal'))
-document.addEventListener('scroll', scrollFunc)
+const reveal = Array.from(document.getElementsByClassName("reveal"));
+document.addEventListener("scroll", scrollFunc);
 
 function scrollFunc() {
-    reveal.forEach((element) => {
-        let obj = ({top, bottom, height} = element.getBoundingClientRect)
-        if(obj.top > 0 && obj.bottom < window.outerHeight - obj.height) {
-            element.classList.add('reveal_active')
-        } else {
-            element.classList.remove('reveal_active')
-        }
-    })
+  reveal.forEach((element) => {
+    let obj = ({ top, bottom, height } = element.getBoundingClientRect());
+    if (obj.top > 0 && obj.bottom < window.outerHeight - obj.top) {
+      element.classList.add("reveal_active");
+    } else {
+      element.classList.remove("reveal_active");
+    }
+  });
 }
